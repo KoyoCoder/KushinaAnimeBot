4c9e70da0c97a281d7d413efd17a422851587cc5
AARUSH <77516086+KoyoCoder@users.noreply.github.com>
2022-01-13 08:20:44 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/StarkBug/KushinaAnimeBot.git /KushinaAnimeBot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /KushinaAnimeBot
+fi
+cd /KushinaAnimeBot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 ./KushinaAnimeBot/__int__.py
